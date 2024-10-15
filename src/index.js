803ecc3ce3caf2f9426253ad2b51db059029d687
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-10-15 20:10:06 +0200

    Setup handlebars

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const app = express();
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }))
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
 
 
 app.get('/', (req, res) => {
-    res.send('Just Works!!');
+    res.render('index', {layout: false});
 })
 
 app.listen( 1000, () =>  console.log('Server listening on http://localhost:1000'));
