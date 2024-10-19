d36f296133b82e4351eb5d3f9b8803d88f27749c
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-10-19 21:13:54 +0200

    Add DB Connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import express  from 'express';
-import handlebarsInit from './config/handlebarsInit.js';
+import handlebarsInit from './config/handlebarsinit.js';
 
 import routes from './routes.js';
 import expressInit from './config/expressInit.js';
+import mongooseInit  from './config/mongooseInit.js';
 
 const app = express();
 
 handlebarsInit(app);
 expressInit(app);
+mongooseInit();
 
 app.use(routes);
 
